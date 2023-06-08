365c02aacfc6ec52f0e17db3ccb1412aa48e9fb8
harshasatpute <harshasatpute8@gmail.com>
2023-06-08 14:57:27 +0530

    First commit

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿
+using System;
+
+namespace MyApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to MyApp!");
+
+            // Your application logic here
+
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+        }
+    }
+}
